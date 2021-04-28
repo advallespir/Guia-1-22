d9f65ca87c16e3978d2e6bed978664b768a6f750
ADVallespir <advallespir@gmail.com>
2021-04-28 10:13:04 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Guia_1_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var DiaYFecha = DateTime.Now;
+            var SoloDia = DiaYFecha.ToShortDateString(); ;
+            Console.WriteLine($"{SoloDia}");
+            Console.ReadKey();
+        }
+    }
+}
